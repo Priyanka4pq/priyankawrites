780375d6ba6400d45dedee7d641e6a9a424dd18f
Priyanka Prajapati <priyankanp@acem.edu.in>
2025-03-29 18:05:00 +0530

    working on nav

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,120 +1,4 @@
-// import React, { useState } from "react";
-// import { Link, NavLink } from "react-router-dom";
-// import { IoMenu } from "react-icons/io5";
-// import { IoClose } from "react-icons/io5";
-// import { useDispatch, useSelector } from "react-redux";
-// import avatarImg from "../assets/hero-carosel/user.jpg";
-// import { useLogoutUserMutation } from "../redux/features/auth/authApi";
-// import { logout } from "../redux/features/auth/authSlice";
 
-// const navLists = [
-//   { name: "Home", path: "/" },
-//   { name: "About us", path: "/about-us" },
-//   { name: "Privacy Policy", path: "/privacy-policy" },
-//   { name: "Contact Us", path: "/contact-us" },
-// ];
-// const Navbar = () => {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-//   const { user } = useSelector((state) => state.auth);
-//   console.log(user);
-//   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-//   const dispatch = useDispatch();
-//   const [logoutUser] = useLogoutUserMutation();
-
-//   const handleLogout = async () => {
-//     try {
-//       await logoutUser().unwrap();
-//       dispatch(logout());
-//     } catch (error) {}
-//   };
-//   return (
-//     <>
-//       <header className="bg-white py-6 border">
-//         <nav className="container mx-auto flex justify-between px-5 ">
-//           <a href="/">
-//             <img src="/hotel.jpeg" alt="" className="h-12" />
-//           </a>
-//           <ul className="sm:flex hidden items-center gap-8">
-//             {navLists.map((list, index) => (
-//               <li key={index}>
-//                 <NavLink
-//                   to={`${list.path}`}
-//                   className={({ isActive }) => (isActive ? "active" : "")}
-//                 >
-//                   {list.name}
-//                 </NavLink>
-//               </li>
-//             ))}
-
-//             {/* render btn based on user login activity */}
-//             {user && user.role === "user" ? (
-//               <li className="flex items-center gap-3">
-//                 <img src={avatarImg} alt="" className="size-8" />
-//                 <button
-//                   className="bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm"
-//                   onClick={handleLogout}
-//                 >
-//                   Logout
-//                 </button>
-//               </li>
-//             ) : (
-//               <li>
-//                 <NavLink to="/login">Login</NavLink>
-//               </li>
-//             )}
-
-//             {user && user.role === "admin" && (
-//               <li className="flex items-center gap-3">
-//                 <img src={avatarImg} alt="" className="size-8" />
-//                 <Link to="/dashboard">
-//                   <button className="bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm">
-//                     Dashboard
-//                   </button>
-//                 </Link>
-//               </li>
-//             )}
-//           </ul>
-
-//           {/* toggle menu */}
-//           <div className="flex items-center sm:hidden">
-//             <button
-//               onClick={toggleMenu}
-//               className="flex items-center px-3 py-4 bg-[#fafafa] rounded text-sm text-gray-500 hover:text-gray-900"
-//             >
-//               {isMenuOpen ? (
-//                 <IoClose className="size-6" />
-//               ) : (
-//                 <IoMenu className="size-6" />
-//               )}
-//             </button>
-//           </div>
-//         </nav>
-
-//         {/* mobile menu */}
-//         {isMenuOpen && (
-//           <ul className="fixed top-[108px] left-0 w-full h-auto pb-8 border-b bg-white shadow-sm z-50">
-//             {navLists.map((list, index) => (
-//               <li className="mt-5 px-4" key={index}>
-//                 <NavLink
-//                   onClick={() => setIsMenuOpen(false)}
-//                   to={`${list.path}`}
-//                   className={({ isActive }) => (isActive ? "active" : "")}
-//                 >
-//                   {list.name}
-//                 </NavLink>
-//               </li>
-//             ))}
-//             <li className="px-4 mt-5">
-//               <NavLink to="/login">Login</NavLink>
-//             </li>
-//           </ul>
-//         )}
-//       </header>
-//     </>
-//   );
-// };
-
-// export default Navbar;
 
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
@@ -255,6 +139,21 @@ const Navbar = () => {
                 Login
               </NavLink>
             </li>
+
+            {user && user.role === "admin" && (
+              <li className="flex items-center gap-3">
+                <img
+                  src={avatarImg}
+                  alt="Admin Avatar"
+                  className="size-8 rounded-full shadow-md border-gray-400"
+                />
+                <Link to="/dashboard">
+                  <button className="bg-gray-600 px-4 py-1.5 text-white font-serif font-semibold rounded-md shadow-lg hover:bg-gray-500 transition-all duration-300">
+                    Dashboard
+                  </button>
+                </Link>
+              </li>
+            )}
           </ul>
         )}
       </header>
